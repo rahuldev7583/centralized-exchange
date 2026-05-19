aa822bf9679d61a4136f25ca5e124f90967e8d8e
rahuldev7583 <rahuldev7583@gmail.com>
2026-05-19 14:45:59 +0530

    initial engine

--- apps/matching-engine/index.ts
+++ apps/matching-engine/index.ts
@@ -1,1 +1,41 @@
-console.log('Hello via Bun!');
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err: any) =>
+  console.log({ msg: 'Redis client error', err }),
+);
+
+client.connect();
+console.log('Connected');
+
+const publishclient = createClient();
+
+publishclient.on('error', (err: any) =>
+  console.log({ msg: 'Redis client error', err }),
+);
+
+publishclient.connect();
+
+console.log('Connected');
+
+while (1) {
+  const res = await client.brPop('create-order', 2);
+
+  if (!res) {
+    continue;
+  }
+  const parsed_res = JSON.parse(res.element);
+  console.log({ parsed_res });
+
+  //  do matching engine, check command type, do matching engine stuff and return price and identifier
+
+  const filled_quantity = 10;
+  const res_data = {
+    identifier: parsed_res.req_id,
+    price: parsed_res.price,
+    filled_quantity,
+  };
+
+  publishclient.lPush('filled-order ', JSON.stringify(res_data));
+}
